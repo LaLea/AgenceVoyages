4f2bdfbb05ebd9ab69aabc94548108bb7b35775f
bailleulb <benoit1.bailleul@etudiant.univ-lille1.fr>
2015-11-20 16:11:43 +0100

    ajojout connection

---
+++ src/BDDConnection.java
@@ -0,0 +1,48 @@
+
+/**
+ * @author vanelle
+ *
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+
+public class BDDConnection {
+	
+	String url = "sql2.olympe.in";
+	String userName = "y5cf3srf";
+	String password = "P@ssw0rd2015";
+	
+	private static BDDConnection INSTANCE = null;
+	
+	private Connection c;
+	
+	public BDDConnection(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			c = DriverManager.getConnection("jdbc:mysql://webtp.fil.univ-lille1.fr:3306/vanelle?user=vanelle&password=mouais^^030792");
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("Erreur classforname");
+		} catch (SQLException e) {
+			System.out.println("Erreur connection \n");
+		}
+	}
+	
+	public static BDDConnection getInstance(){
+		if (INSTANCE == null){
+			return new BDDConnection();
+		}
+		else {
+			return INSTANCE;
+		}
+	}
+	
+	public Connection getC(){
+		return this.c;
+	}
+}
