9f50b8d9c968c3edcb2aa1b7e69ece87202d07fb
LaLea <lea.vanelle@etudiant.univ-lille1.fr>
2015-12-11 00:45:17 +0100

    Modification de la classe tabClient par ajout textfield

--- src/interfaceGraphique/TabClient.java
+++ src/interfaceGraphique/TabClient.java
@@ -2,10 +2,13 @@ package interfaceGraphique;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class TabClient extends JPanel{
 	public TabClient(){
 		super();
 		add(new JLabel("Gestion des clients"));
+		add(new JLabel("Ajout d'un client"));
+		add(new JTextField("Nom :"));
 	}
 }
