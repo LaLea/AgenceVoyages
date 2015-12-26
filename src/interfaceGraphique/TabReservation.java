5c1d647c6350860c22e88c73571aa93ee7ef2751
LaLea <lea.vanelle@etudiant.univ-lille1.fr>
2015-12-26 14:49:47 +0100

    Réalisation de l'affichage de l'interface graphique

--- src/interfaceGraphique/TabReservation.java
+++ src/interfaceGraphique/TabReservation.java
@@ -1,15 +1,145 @@
 package interfaceGraphique;
 
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BoxLayout;
-import javax.swing.JLabel;
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
+import javax.swing.JComboBox;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+import com.toedter.calendar.JDateChooser;
+
+import domaine.Categorie;
+import domaine.Client;
+import domaine.Hotel;
+import domaine.Vol;
 
 public class TabReservation extends JPanel{
+
+	// Barre de recherche
+	private JTextField tfSearch;
+	private JComboBox<String> cbSearch;
+	// Liste de clients
+	private JList<Client> lCust;
+	private DefaultListModel<Client> dlmCust;
+	// Champs a remplir
+	private JComboBox<String> cbVilleDep;
+	private JComboBox<String> cbVilleArr;
+	private JDateChooser dcDteAller;
+	private JDateChooser dcDteRetr;
+	private JTextField tfNbPersAcc;
+	// Boutons Radio
+	private JRadioButton rbPriceOrder;
+	private JRadioButton rbTimeOrder;
+	// Liste de vols
+	private JList<Vol> lVols;
+	private DefaultListModel<Vol> dlmVols;
+	// radio button 1ere et 2eme classe
+	private JRadioButton rb1Classe;
+	private JRadioButton rb2Classe;
+	// les 2 listes hotel + categories de chambres
+	private DefaultListModel<Hotel> dlmHotel;
+	private JList<Hotel> lHotel;
+	private DefaultListModel<Categorie> dlmCat;
+	private JList<Categorie> lCat;
 	
 	public TabReservation(){
 		super();
-		setLayout(new BoxLayout(arg0, arg1));
-		add(new JLabel("Gestion des reservations"));
+		// Creation d'un grand JPanel contenant le titre + JPanel middlePane + Jbutton d'enregistrement de la réservation
+		JPanel general = new JPanel();
+		add(general);
+		general.setLayout(new BoxLayout(general,BoxLayout.PAGE_AXIS));
+		general.add(InterfaceGraphique.createTitle("Gestion des reservations"));
+		JPanel middlePane = new JPanel();
+		general.add(middlePane);
+		general.add(InterfaceGraphique.createOneButton(new bSaveBookingListener(),"Enregistrer la réservation."));
+		// Creation du middlePane contenant un Jpanel a gauche et un Jpanel à droite
+		middlePane.setLayout(new GridLayout(1,2));
+		middlePane.add(createLeftMiddlePane());
+		middlePane.add(createRightMiddlePane());
+	}
+	
+	public JPanel createLeftMiddlePane(){
+		JPanel leftMiddlePane = new JPanel();
+		leftMiddlePane.setLayout(new BoxLayout(leftMiddlePane,BoxLayout.Y_AXIS));
+		// Barre de recherche
+		leftMiddlePane.add(InterfaceGraphique.createSubTitle("Rechercher un client :"));
+		leftMiddlePane.add(InterfaceGraphique.createSearchCust(tfSearch, new bSearchReservListener(), cbSearch));
+		// Liste des clients
+		leftMiddlePane.add(InterfaceGraphique.createSubTitle("Les clients :"));
+		leftMiddlePane.add(InterfaceGraphique.createListCust(dlmCust, lCust, 60, 150));
+		// Boutons
+		leftMiddlePane.add(InterfaceGraphique.createButtonsPair(new bDeselCustListener(),new bAffCustListener()));
+		//Champs a remplir
+		leftMiddlePane.add(InterfaceGraphique.createCBVille("Ville de départ* :", cbVilleDep));
+		leftMiddlePane.add(InterfaceGraphique.createCBVille("Ville d'arrivée :", cbVilleArr));
+		leftMiddlePane.add(InterfaceGraphique.createDateChooser("Date de départ :",dcDteAller));
+		leftMiddlePane.add(InterfaceGraphique.createDateChooser("Date de retour :",dcDteRetr));
+		leftMiddlePane.add(InterfaceGraphique.createInputBox("Nombre de personnes accompagnantes :", 2, tfNbPersAcc));
 		
+		return leftMiddlePane;
+	}
+
+	public JPanel createRightMiddlePane() {
+		JPanel rightMiddlePane = new JPanel();
+		rightMiddlePane.setLayout(new BoxLayout(rightMiddlePane, BoxLayout.Y_AXIS));
+		//En tete de liste
+		rightMiddlePane.add(InterfaceGraphique.createGroupRadioButton("Les vols possibles : ",rbPriceOrder, "Tier par prix", rbTimeOrder, "Trier par temps de voyage",true));
+		// La liste
+		rightMiddlePane.add(InterfaceGraphique.createListVol(dlmVols, lVols, 60, 150));
+		// A voir pour l'ajout du bouton deselectionner tout si nécessaire
+		rightMiddlePane.add(InterfaceGraphique.createGroupRadioButton("Classe :", rb1Classe, "1ère classe", rb2Classe, "2ème classe",true));
+		rightMiddlePane.add(InterfaceGraphique.createSubTitle("Les hébergements possibles :"));
+		// Les 2 listes hotels et catégories de chambres
+		JPanel twoListPane = new JPanel();
+		rightMiddlePane.add(twoListPane);
+		twoListPane.setLayout(new GridLayout(1,2));
+		JPanel leftTwoListPane = new JPanel();
+		leftTwoListPane.setLayout(new BoxLayout(leftTwoListPane, BoxLayout.Y_AXIS));
+		JPanel rightTwoListPane = new JPanel();
+		rightTwoListPane.setLayout(new BoxLayout(rightTwoListPane, BoxLayout.Y_AXIS));
+		twoListPane.add(leftTwoListPane);
+		twoListPane.add(rightTwoListPane);
+		leftTwoListPane.add(InterfaceGraphique.createListHotel(dlmHotel, lHotel, 30, 150));
+		rightTwoListPane.add(InterfaceGraphique.createListCat(dlmCat, lCat, 30, 150));
+		return rightMiddlePane;
+	}
+
+	
+	
+	private class bSearchReservListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+		}
+	}
+	
+	private class bDeselCustListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	private class bAffCustListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+		}
+	}
+	
+	private class bSaveBookingListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+		}
 	}
 }
--- src/interfaceGraphique/TabVoyage.java
+++ src/interfaceGraphique/TabVoyage.java
@@ -1,12 +1,140 @@
 package interfaceGraphique;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+import domaine.Client;
+import domaine.Voyage;
 
 public class TabVoyage extends JPanel{
 
+	private JTextField tfSearchCust;
+	private JComboBox<String> cbSearchCust;
+	private JList<Client> lCust;
+	private DefaultListModel<Client> dlmCust;
+	private JList<Voyage> lVoyage;
+	private DefaultListModel<Voyage> dlmVoyage;
+	private JTextField tfVilleDepVol;
+	private JTextField tfVilleArrVol;
+	private JTextField tfJourVol;
+	private JTextField tfHHeureVol;
+	private JTextField tfMnHeureVol;
+	private JTextField tfHDureeVol;
+	private JTextField tfMnDureeVol;
+	private JRadioButton rb1Classe;
+	private JRadioButton rb2Classe;
+	private JTextField tfTarifVol;
+	private JTextField tfVilleHotel;
+	private JTextField tfNomHotel;
+	private JTextField tfCatChbre;
+	private JTextField tfNbPers;
+
 	public TabVoyage(){
 		super();
-		add(new JLabel("Gestion des voyages"));
+		JPanel midPane = new JPanel();
+		add(midPane);
+		midPane.add(createLeftMidPane(),JPanel.BOTTOM_ALIGNMENT);
+		midPane.add(createMiddleMidPane(),JPanel.BOTTOM_ALIGNMENT);
+		midPane.add(createRightMidPane(),JPanel.BOTTOM_ALIGNMENT);
+		//add(InterfaceGraphique.createOneButton(new AnnulReservListener(), "Annuler la réservation"));
+	}
+	
+	private class AnnulReservListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	private JPanel createRightMidPane() {
+		JPanel rightMidPane = new JPanel();
+		rightMidPane.setLayout(new BoxLayout(rightMidPane, BoxLayout.Y_AXIS));
+		rightMidPane.add(InterfaceGraphique.createSubTitle("Le vol :"));
+		rightMidPane.add(InterfaceGraphique.createInputBox("Ville de départ :", 20, tfVilleDepVol));
+		rightMidPane.add(InterfaceGraphique.createInputBox("Ville d'arrivée :", 20, tfVilleArrVol));
+		rightMidPane.add(InterfaceGraphique.createInputBox("Jour :", 20, tfJourVol));
+		JPanel heure = new JPanel();
+		heure.add(InterfaceGraphique.createInputBox("Heure :", 2, tfHHeureVol));
+		heure.add(InterfaceGraphique.createInputBox(" : ", 2, tfMnHeureVol));
+		rightMidPane.add(heure);
+		JPanel duree = new JPanel();
+		duree.add(InterfaceGraphique.createInputBox("Durée :", 2, tfHDureeVol));
+		duree.add(InterfaceGraphique.createInputBox(" : ", 2, tfMnDureeVol));
+		rightMidPane.add(duree);
+		rightMidPane.add(InterfaceGraphique.createGroupRadioButton("Classe :", rb1Classe, "1ère classe", rb2Classe, "2ème classe",false));
+		rightMidPane.add(InterfaceGraphique.createInputBox("Tarif :", 20, tfTarifVol));
+		
+		rightMidPane.add(InterfaceGraphique.createSubTitle("L'hébergement :"));
+		rightMidPane.add(InterfaceGraphique.createInputBox("Ville :", 20, tfVilleHotel));
+		rightMidPane.add(InterfaceGraphique.createInputBox("Nom hotel :", 20, tfNomHotel));
+		rightMidPane.add(InterfaceGraphique.createInputBox("Catégorie de la chambre :", 20, tfCatChbre));
+		rightMidPane.add(InterfaceGraphique.createInputBox("Nombre de personnes supplémentaires :", 2, tfNbPers));
+		rightMidPane.add(InterfaceGraphique.createOneButton(new AnnulReservListener(), "Annuler la réservation"));
+		return rightMidPane;
+	}
+
+	private JPanel createMiddleMidPane() {
+		JPanel middleMidPane = new JPanel();
+		middleMidPane.setLayout(new BoxLayout(middleMidPane, BoxLayout.Y_AXIS));
+		middleMidPane.add(InterfaceGraphique.createSubTitle("Les voyages du client :"));
+		middleMidPane.add(InterfaceGraphique.createListVoyage(dlmVoyage, lVoyage, 60, 200));
+		middleMidPane.add(InterfaceGraphique.createOneButton(new DeselVoyageListener(), "Désélectionner tout"));
+		return middleMidPane;
+	}
+
+	private JPanel createLeftMidPane() {
+		JPanel leftMidPane = new JPanel();
+		leftMidPane.setLayout(new BoxLayout(leftMidPane, BoxLayout.Y_AXIS));
+		leftMidPane.add(InterfaceGraphique.createSubTitle("Rechercher un client :"));
+		leftMidPane.add(InterfaceGraphique.createSearchCust(tfSearchCust, new SearchCustListener(), cbSearchCust));
+		leftMidPane.add(InterfaceGraphique.createSubTitle("Les clients :"));
+		leftMidPane.add(InterfaceGraphique.createListCust(dlmCust, lCust, 60, 200));
+		leftMidPane.add(InterfaceGraphique.createButtonsPair(new DeselCustListener(), new AffToutCustListener()));
+		return leftMidPane;
+	}
+	
+	private class SearchCustListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	private class DeselCustListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	private class AffToutCustListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	private class DeselVoyageListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 	}
 }
